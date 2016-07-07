95acf44d85fb580aaf80690aef6de570d70252c1
Richard Reitzfeld <richie.reitzfeld@gmail.com>
2016-07-06 22:10:08 -0400

    sets up RDSCDataSourceChanges

--- RDSCDataSourceChanges/Classes/RDSCDataSourceChanges.h
+++ RDSCDataSourceChanges/Classes/RDSCDataSourceChanges.h
@@ -8,6 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
-@interface RDSCDataSourceChanges : NSObject
+
+
+
+
+@interface RDSCDataSourceChanges<ObjectType> : NSObject
+
+#pragma mark - Objects
+@property (nonatomic, readonly, nullable) NSSet<ObjectType>* deletedObjects;
+@property (nonatomic, readonly, nullable) NSSet<ObjectType>* insertedObjects;
+
+#pragma mark - Init Methods
+-(nonnull instancetype)initWithDeletedObjects:(nullable NSSet<ObjectType>*)deletedObjects
+insertedObjects:(nullable NSSet<ObjectType>*)insertedObjects NS_DESIGNATED_INITIALIZER;
 
 @end
--- RDSCDataSourceChanges/Classes/RDSCDataSourceChanges.m
+++ RDSCDataSourceChanges/Classes/RDSCDataSourceChanges.m
@@ -8,6 +8,30 @@
 
 #import "RDSCDataSourceChanges.h"
 
+
+
+
+
 @implementation RDSCDataSourceChanges
+
+#pragma mark - NSObject
+-(instancetype)init
+{
+  return (self = [self initWithDeletedObjects:nil
+                              insertedObjects:nil]);
+}
+
+#pragma mark - Init Methods
+-(nonnull instancetype)initWithDeletedObjects:(nullable NSSet*)deletedObjects
+                              insertedObjects:(nullable NSSet*)insertedObjects
+{
+  if (self = [super init])
+  {
+    _deletedObjects = [deletedObjects copy];
+    _insertedObjects = [insertedObjects copy];
+  }
+  
+  return self;
+}
 
 @end
